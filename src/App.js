425dc7f7908a9a83ff70d56055ae417a9d55826f
Aditya Singh <singhanshu9724@gmal.com>
2024-03-30 13:21:08 +0530

    signIn & signup form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-
-import './App.css';
-
+import Body from './Components/Body';
 function App() {
   return (
     <div className="  text-7xl  font-bold text-green-800  uppercase text-center">
-     hello, lets build the netflix  
+     <Body/>
     </div>
   );
 }
